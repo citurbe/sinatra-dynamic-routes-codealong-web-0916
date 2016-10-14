8e27e328e0624ff397412f758c4d8ba4f3ec0adc
citurbe <craigiturbe@Craigs-MacBook-Air.local>
2016-10-14 09:58:34 -0400

    add routes for goodbye and multiply

--- app.rb
+++ app.rb
@@ -13,5 +13,15 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye #{@user_name}!"
+  end
+
+  get '/multiply/:first_num/:second_num' do
+    @product = params[:first_num].to_i * params[:second_num].to_i
+    "The product is #{@product}"
+  end
+
 
 end
